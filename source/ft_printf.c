bb7fa46b772cafd34823ca6acf31eff579dc7598
Atoomsnor <verschoor.rowan@gmail.com>
2024-11-08 16:14:15 +0100

    yee

--- source/ft_printf.c
+++ source/ft_printf.c
@@ -6,39 +6,73 @@
 /*   By: roversch <roversch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:40:30 by roversch          #+#    #+#             */
-/*   Updated: 2024/11/07 16:10:20 by roversch         ###   ########.fr       */
+/*   Updated: 2024/11/08 15:58:16 by roversch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
+
+int	ft_printchar(char c)
+{
+	write (1, &c, 1);
+	return (1);
+}
+
+int	ft_format(const char format, va_list args)
+{
+	int	printlen;
+
+	printlen = 0;
+	if (format == 'c')
+		ft_printchar(va_arg(args, int));
+	// else if (format == 's')
+	// 	ft_printstr(va_arg(args, char *));
+	// else if (format == 'p')
+	// 	ft_printvoid(va_arg(args, void *));
+	// else if (format == 'd' || format == 'i')
+	// 	ft_printint(va_arg(args, int));
+	// else if (format == 'u')
+	// 	ft_print_unsign_deci(va_arg(args, unsigned int));
+	// else if (format == 'x')
+	// 	ft_print_low_hexa(va_arg(args, unsigned int));
+	// else if (format == 'X')
+	// 	ft_print_up_hexa(va_arg(args, unsigned int));
+	else if (format == '%')
+		ft_printchar('%');
+	return (printlen);
+}
 
 int	ft_printf(const char *str, ...)
 {
-	size_t	i;
-
+	int	i;
+	int	printlen;
+	va_list	args;
+	
 	i = 0;
-	while (*str)
+	printlen = 0;
+	va_start(args, str);
+	while (str[i])
 	{
-		if (*str == '%')
-			"do something cool";
+		if (str[i] == '%')
+		{	
+			printlen += ft_format(str[i + 1], args);
+			i++;
+		}
 		else 
-			ft_putchar
-		write (1, str, 1);
-		str++;
+			printlen += ft_printchar(str[i]);
 		i++;
 	}
-	return (0);
+	va_end(args);
+	return (printlen);
 }
 
 int	main()
 {
-	ft_printf("hello");
-	
+	int	len;
+
+	len = ft_printf("Hello %c kek\n", 'W');
+	printf("Chars prined: %d\n", len);
 	return (0);
 }
-
-char *str = "jatoch";
-int	getal = 12;
-
-printf("print de string: %s, print een getal:%d", str, getal)
